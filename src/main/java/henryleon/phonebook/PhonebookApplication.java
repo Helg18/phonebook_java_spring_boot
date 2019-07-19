06db4266cb4e8f4a84c498483e15c218e9968149
Henry Leon Gomez <helg18@gmail.com>
2019-07-19 14:02:29 -0400

    First Commit

---
+++ src/main/java/henryleon/phonebook/PhonebookApplication.java
@@ -0,0 +1,13 @@
+package henryleon.phonebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonebookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhonebookApplication.class, args);
+    }
+
+}
