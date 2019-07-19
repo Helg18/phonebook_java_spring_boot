931b7f8ee8c0a90898e096372f757b01d3c590bb
Henry Leon Gomez <helg18@gmail.com>
2019-07-19 14:07:36 -0400

    Create Contac Class

---
+++ src/main/java/henryleon/phonebook/Contact.java
@@ -0,0 +1,52 @@
+package henryleon.phonebook;
+
+public class Contact {
+    private String nombre;
+    private String apellido;
+    private String email;
+    private long telefono;
+
+    public Contact(String nombre, String apellido, String email, long telefono) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(long telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getFullName() {
+        return this.getNombre() + " " + this.getApellido();
+    }
+
+}
